371270cc2e697ce3507f42dcd8e8e7b1e421d0dd
v-i-s-h-a-l <5095154+v-i-s-h-a-l@users.noreply.github.com>
2026-06-04 19:14:52 +0530

    platform: upgrade minimum platforms to iOS 18+ / macOS 15+ / tvOS 18+ / watchOS 11+ / visionOS 2+
    - Update Package.swift platform requirements
    - Update README platform badges
    - Remove obsolete @available / #available annotations
    - Update CI matrix (AnvilRunner: drop macos-14)
    
    BREAKING CHANGE: minimum platforms raised per policy.platform

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "AnvilRunner",
     platforms: [
-        .macOS(.v14)
+        .macOS(.v15)
     ],
     products: [
         .library(name: "AnvilRunner", targets: ["AnvilRunner"]),
